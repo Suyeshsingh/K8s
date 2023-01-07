fb16ea9a3728f35a531352ba1b3c31228013d7d7
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2023-01-07 21:42:55 +0545

    Update project_creo.sh

--- project_creo.sh
+++ project_creo.sh
@@ -1,13 +1,38 @@
 #This project is being developed and maintained by Suyesh Singh.
-#Commit no. 1
+#Commit no. 5
 #Commit date 07 Jan 2023
 
-echo "Please enter the namespace: "
-echo "then press[ENTER]:" 
+echo "Please enter the namespace(Please do not use just plain numbers as namespace, api version will not read it.): "
+echo "then press[ENTER]:"
 read namespace
 
 echo "Creating namespace $namespace. "
 
 kubectl create ns $namespace
 
-echo "$namespace created."
+echo "Do you want to create a secret?"
+read -p "Enter your choice. Yes(y) No(any other key) " choice
+if [ "$choice" = "y" ]; then
+echo "Creating secret file"
+
+cat >> $namespace-secret.yaml <<EOF
+
+apiVersion: v1
+kind: Secret
+metadata:
+  name: $namespace-secret
+  namespace: $namespace
+type: Opaque
+data:
+  USER_NAME: YWRtaW4=
+  PASSWORD: MWYyZDFlMmU2N2Rm
+
+EOF
+
+kubectl apply -f $namespace-secret.yaml
+
+else
+echo "Not creating a secret"
+echo "Thank you for using Creo."
+fi
+
