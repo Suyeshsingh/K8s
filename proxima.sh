3c89a3f5a54e01a0aaa2e63303c7b0b8773ff6c1
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2023-01-07 21:19:15 +0545

    Update proxima.sh

--- proxima.sh
+++ proxima.sh
@@ -1,4 +1,7 @@
 #This project is being developed and maintained by Suyesh Singh.
+#Commit no. 1
+#Commit date 07 Jan 2023
+
 echo "Please enter the namespace: "
 echo "then press[ENTER]:" 
 read namespace
