d27b7fe211b9642e413171526d0693d8dbb1a61e
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2023-01-08 13:30:14 +0545

    Update creo_service.sh

--- Service/creo_service.sh
+++ Service/creo_service.sh
@@ -1,1 +1,7 @@
-test
+#This project is being developed and maintained by Suyesh Singh.
+#Commit no. 01
+#Commit date 08 Jan 2023
+
+echo "Please enter the namespace(Please do not use just plain numbers as namespace, if you do so yaml file will not be applied.): "
+echo "then press[ENTER]:"
+read namespace
