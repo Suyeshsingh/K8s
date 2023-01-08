d07719c83dc04bbd9567d29a7921477d9bcd5836
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2023-01-08 13:59:59 +0545

    Update creo_service.sh

--- Service/creo_service.sh
+++ Service/creo_service.sh
@@ -5,3 +5,25 @@
 echo "Please enter the namespace(Please do not use just plain numbers as namespace, if you do so yaml file will not be applied.): "
 echo "then press[ENTER]:"
 read namespace
+
+apiVersion: v1
+kind: Service
+metadata:
+  name: finpos-configurations
+  namespace: finpos-dev
+  annotations:
+    isDiscoverable: 'true'
+    servicePatterns: settings,systems
+spec:
+  ports:
+    - name: http
+      protocol: TCP
+      port: 8080
+      targetPort: 8080
+  selector:
+    app: finpos-configurations
+  type: ClusterIP
+  sessionAffinity: None
+  ipFamilies:
+    - IPv4
+  ipFamilyPolicy: SingleStack
