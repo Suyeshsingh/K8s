29d649006c1fc1148e9ba17a67a072e330466e06
Suyesh Singh <40593135+Suyeshsingh@users.noreply.github.com>
2023-01-08 09:02:49 +0545

    Update creo_ingress.sh

--- Ingress/creo_ingress.sh
+++ Ingress/creo_ingress.sh
@@ -1,1 +1,11 @@
-test
+#This project is being developed and maintained by Suyesh Singh.
+#Commit no. 07
+#Commit date 08 Jan 2023
+
+echo "Please enter the namespace(Please do not use just plain numbers as namespace, if you do so yaml file will not be applied.): "
+echo "then press[ENTER]:"
+read namespace
+
+#echo "Creating namespace $namespace. " #Enable this for testing purpose only.
+
+#kubectl create ns $namespace #Enable this for testing purpose only.
